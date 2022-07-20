7f9daa7c6b64055688c5c9e2951a55569b0aa8ef
Manuel Leiner <manuel.leiner@naix.de>
2022-07-20 12:10:18 +0200

    Initial commit

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use App\Models\Team;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
+ */
+class ProjectFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'team_id' => Team::factory(),
+            'name'    => fake()->name(),
+        ];
+    }
+}
